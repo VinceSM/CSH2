ac36f4eb3137c7b9bbd146000c92f3aa07e4dd8b
Vicente SM <vsmx427@gmail.com>
2024-05-21 23:33:29 -0300

    Agregacion Model, Interface, View

---
+++ Model/RobotDriver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Transporte2.Interface;
+
+namespace Transporte2.Model
+{
+    public class RobotDriver : IDriver
+    {
+        public string Navigate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Model/Transport.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Transporte2.Interface;
+
+namespace Transporte2.Model
+{
+    public class Transport
+    {
+        public IEngine engine {  get; set; }
+        public IDriver driver { get; set; }
+
+        public void Deliver(string destination, string cargo)
+        {
+
+        }
+    }
+}
